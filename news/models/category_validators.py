1fa00b138db4d198769c7058670a2b1da13204e1
calebeLAR <ca6ele@gmail.com>
2023-10-10 14:46:59 -0300

    refat: altera mensagem de erro em validate_name_field

--- news/models/category_validators.py
+++ news/models/category_validators.py
@@ -3,18 +3,4 @@ from django.core.exceptions import ValidationError
 
 def validate_name_field(value):
     if len(value) == 0 or value is None:
-        raise ValidationError("invalid name: name cannot be empty")
-
-
-# def v(vale):
-#     if not vale:
-#         print("if not vale")
-
-#     if len(vale) == 0:
-#         print("len(vale) == 0")
-
-#     if vale is None:
-#         print("if vale is None")
-
-
-# v("")
+        raise ValidationError("invalid value: name cannot be empty")
