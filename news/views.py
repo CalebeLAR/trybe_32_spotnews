e91de6701c26151eed06a4595998dd07aab8ad11
calebeLAR <ca6ele@gmail.com>
2023-10-11 11:03:57 -0300

    req05:feat: cria template da pagina de detalhes

--- news/views.py
+++ news/views.py
@@ -5,3 +5,8 @@ from news.models.news_model import News
 def home_page(request):
     context = {"news": News.objects.all()}
     return render(request, "home.html", context=context)
+
+
+def detais_page(request, id):
+    context = {"new": News.objects.get(id=id)}
+    return render(request, "news_details.html", context=context)
