6e9ab13ec0c5f9274ec18246799ceec84d1d4fbf
calebeLAR <ca6ele@gmail.com>
2023-10-11 11:03:30 -0300

    req05:feat: inclui rotas para pagina de detalhes

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from news.views import home_page
+from news.views import detais_page, home_page
 
 urlpatterns = [
     path("", home_page, name="home-page"),
+    path("news/<int:id>", detais_page, name="news-details-page"),
 ]
