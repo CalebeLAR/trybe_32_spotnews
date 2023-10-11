8fa7a509da631155f9f53e666ae449205a5948d5
calebeLAR <ca6ele@gmail.com>
2023-10-10 22:35:50 -0300

    req04: implemente view home-page

--- news/views.py
+++ news/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from news.models.news_model import News
 
-# Create your views here.
+
+def home_page(request):
+    context = {"news": News.objects.all()}
+    return render(request, "home.html", context=context)
