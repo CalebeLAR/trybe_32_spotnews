42538ea32f5f05d5f3f92a6e27ab13036306e924
calebeLAR <ca6ele@gmail.com>
2023-10-11 15:47:58 -0300

    req06: cria view para pagina de formulario de categoria

--- news/views.py
+++ news/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from news.models.category_model import Categories
 from news.models.news_model import News
+from news.forms import CreateCategoryForm
 
 
 def home_page(request):
@@ -12,6 +14,16 @@ def details_page(request, id):
     return render(request, "news_details.html", context=context)
 
 
-def categories_form_page(request, id):
-    context = {"new": News.objects.get(id=id)}
+def categories_form_page(request):
+    form = CreateCategoryForm()
+
+    if request.method == "POST":
+        form = CreateCategoryForm(request.POST)
+
+        if form.is_valid():
+            Categories.objects.create(**form.cleaned_data)
+
+            return redirect("home-page")
+
+    context = {"form": form}
     return render(request, "categories_form.html", context=context)
